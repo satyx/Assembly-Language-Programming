a794f6b6113403d43a085be77d645029be29d426
satyx <satyamcrj@gmail.com>
2020-10-18 23:26:24 +0530

    Substring Check

--- substr.asm
+++ substr.asm
@@ -1,80 +1,94 @@
 .model small
 .stack 100h
 .data
-	l1 db 0
-	l2 db 0
-	txt db 20 dup(?)
-	pat db 20 dup(?)
+
+str db 30 dup(?)
+pat db 20 dup(?)
+success db 'Success$'
+failure db 'Failure$'
+l1 db 0
+l2 db 0
 .code
 MAIN PROC
-	
+	MOV AX,@data
+	Mov DS,AX
+	LEA SI,str
+	LEA DI,l1
+	call inp
+	LEA DI,l2
+	LEA SI,pat
+	call inp
 
-	Mov AX,@data
-	MOv DS,AX
-	LEA SI,txt
-	Mov AH,1
+	Mov AL,l1
+	cmp AL,l2
+	jl nfound
+
 	XOR CX,CX
-	lp1:
-		Int 33
-		cmp AL,0DH
-		je storeTxt
-		Mov [SI], AL
-		Inc SI
-		Inc CX
-		jmp lp1
-	storeTxt:
-		Mov [SI],'$'
-		Inc CX
-		Mov l1,CL
-		XOR CX,CX
-		LEA SI,pat
-	
-	lp2:
-		Int 33
-		cmp AL,0DH
-		je storePat
-		Mov [SI], AL
-		Inc SI
-		Inc CX
-		jmp lp2
-	storePat:
-		Mov [SI],'$'
-		Inc CX
-		Mov l2,CL
-	Mov AH,9
-	LEA DX,txt
-	Int 33
-	LEA DX,pat
-	Int 33
-	
-	XOR CX,CX
-	XOR BX,BX
 	Mov CL,l1
 	Sub CL,l2
-	LEA DI,txt
-	LEA SI,pat
-	txtloop:
-		Push BX
-		XOR SI,SI
-		patloop:
-			XOR BH,BH
-			cmp [DI+BL],[SI+BH]
-			je l02
-			INC BH
-			INC BL
-			cmp BH,l2
-			jl patloop
-		l02:
-		cmp BH,l2
-		je l03
-		Pop BX
-		Inc BL
-		cmp BL,CL
-		jle txtloop
 	
-	l03:
+	XOR AX,AX
+	XOR BX,BX
+	
+	LEA SI,str
+	LEA DI,pat
+	
+	lstart:
+		XOR BX,BX
+		XOR CH,CH
+		lnest:
+			XOR DL,DL
+			Mov DL,[BX+SI]
+			cmp DL,[BX+DI]
+			jne unmatch
+			Inc BX
+			cmp BL,l2
+			jl lnest
+			jmp nxtiter
+			unmatch:
+				Mov CH,1
+		nxtiter:
+			cmp CH,0
+			je found
+			Inc AX
+			Inc SI
+			cmp AL,CL
+			jl lstart
+	jmp nfound
+			
+	found:
+		LEA DX,success
+		jmp display
+	nfound:
+		LEA DX,failure
+	display:
+		Mov AH,9
+		Int 21H
 	Mov AH,76
 	Int 21H
-
 MAIN ENDP
+
+INP PROC
+	Push AX
+	Push SI
+	Push DI
+	XOR CX,CX
+	Mov AH,1
+	rec:
+		Int 21h
+		cmp AL,0DH
+		je restore
+		Mov [SI],AL
+		INC SI
+		Inc CX
+		jmp rec
+	restore:
+		Mov [SI],'$'
+	Mov [DI],CL
+	Pop DI
+	Pop SI
+	Pop AX
+	ret
+INP ENDP
 END
+
